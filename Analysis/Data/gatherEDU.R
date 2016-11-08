342c4bbda8a8245450d792c167ec83d572c37eea
blawrenceDS <blawrence@smu.edu>
2016-11-08 17:06:16 -0600

    Delete gatherEDU.R

--- Analysis/Data/gatherEDU.R
+++
@@ -1,5 +0,0 @@
-library(data.table)
-
-rawEDUdata <- fread('https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv')
-
-tidyEDUdata <- subset(rawEDUdata, select = c("CountryCode","Income Group"))
